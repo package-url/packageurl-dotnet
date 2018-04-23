8eb4c64d15b3e1ab824e10c8db88232fd6e9e873
Adeel <adeelbm@outlook.com>
2018-04-23 15:59:25 +0300

    Add unit tests

---
+++ tests/PackageUrl.Tests.cs
@@ -0,0 +1,84 @@
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using PackageUrl.Tests.TestAssets;
+using Xunit;
+
+namespace PackageUrl.Tests
+{
+    /// <summary>
+    /// Test cases for PackageURL parsing.
+    /// </summary>
+    /// <remarks>
+    /// Original test cases retrieved from: https://raw.githubusercontent.com/package-url/purl-spec/master/test-suite-data.json
+    /// </remarks>
+    public class PackageURLTest
+    {
+        [Theory]
+        [PurlTestData("TestAssets/test-suite-data.json")]
+        public void TestConstructorParsing(PurlTestData data)
+        {
+            if (data.IsInvalid)
+            {
+                Assert.Throws<MalformedPackageUrlException>(() => new PackageURL(data.Purl));
+                return;
+            }
+
+            PackageURL purl = new PackageURL(data.Purl);
+            Assert.Equal(data.CanonicalPurl, purl.ToString());
+
+            Assert.Equal("pkg", purl.Scheme);
+            Assert.Equal(data.Type, purl.Type);
+            Assert.Equal(data.Namespace, purl.Namespace);
+            Assert.Equal(data.Name, purl.Name);
+            Assert.Equal(data.Version, purl.Version);
+            Assert.Equal(data.Subpath, purl.Subpath);
+            if (data.Qualifiers != null)
+            {
+                Assert.NotNull(purl.Qualifiers);
+            }
+        }
+
+        [Theory]
+        [PurlTestData("TestAssets/test-suite-data.json")]
+        public void TestConstructorParameters(PurlTestData data)
+        {
+            if (data.IsInvalid)
+            {
+                Assert.Throws<MalformedPackageUrlException>(() => new PackageURL(data.Purl));
+                return;
+            }
+
+            PackageURL purl = new PackageURL(data.Type, data.Namespace, data.Name, data.Version, data.Qualifiers, data.Subpath);
+
+            Assert.Equal(data.CanonicalPurl, purl.ToString());
+            Assert.Equal("pkg", purl.Scheme);
+            Assert.Equal(data.Type, purl.Type);
+            Assert.Equal(data.Namespace, purl.Namespace);
+            Assert.Equal(data.Name, purl.Name);
+            Assert.Equal(data.Version, purl.Version);
+            Assert.Equal(data.Subpath, purl.Subpath);
+            if (data.Qualifiers != null)
+            {
+                Assert.NotNull(purl.Qualifiers);
+            }
+        }
+    }
+}
---
+++ tests/TestAssets/PurlTestData.cs
@@ -0,0 +1,79 @@
+﻿// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Xunit.Sdk;
+
+namespace PackageUrl.Tests.TestAssets
+{
+    public class PurlTestData : DataAttribute
+    {
+        private static readonly JsonSerializer s_serializer = new JsonSerializer();
+        private static readonly Dictionary<string, IEnumerable<object[]>> s_assetsStore = new Dictionary<string, IEnumerable<object[]>>();
+        private readonly string _filePath;
+
+        public string Description;
+
+        public string Purl;
+
+        [JsonProperty("canonical_purl")]
+        public string CanonicalPurl;
+
+        public string Type;
+
+        public string Namespace;
+
+        public string Name;
+
+        public string Version;
+
+        public SortedDictionary<string, string> Qualifiers;
+
+        public string Subpath;
+
+        [JsonProperty("is_invalid")]
+        public bool IsInvalid;
+
+        public PurlTestData(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            if (s_assetsStore.ContainsKey(_filePath))
+            {
+                return s_assetsStore[_filePath];
+            }
+
+            using (var streamReader = new StreamReader(_filePath))
+            {
+                var reader = new JsonTextReader(streamReader);
+                var data = s_serializer.Deserialize<PurlTestData[]>(reader);
+                s_assetsStore[_filePath] = data.Select(x => new object[] { x });
+                return s_assetsStore[_filePath];
+            }
+        }
+    }
+}
