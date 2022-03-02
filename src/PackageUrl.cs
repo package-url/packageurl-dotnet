53bb00969f8a6f1106545ff0d0e76aae757e1428
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-03-02 10:44:02 -0800

    Update PackageUrl.cs

--- src/PackageUrl.cs
+++ src/PackageUrl.cs
@@ -270,20 +270,16 @@ namespace PackageUrl
             {
                 throw new MalformedPackageUrlException("The PackageURL name specified is invalid");
             }
+            // These repositorys are case sensitive and require the original casing.
+            if (Type == "nuget" || Type == "cocoapods" || Type == "cpan" || Type == "vsm" || Type == "cran")
+            {
+                return name;
+            }
             if (Type == "pypi")
             {
                 name = name.Replace('_', '-');
             }
-            if (Type == "nuget")
-            {
-                return name;
-            }
-            // These repositorys are case sensitive and require the original casing.
-            // For example, the package location for cocoapods is determined by the first 3 bytes of an MD5 of the case sensitive name
-            if (Type == "cocoapods" || Type == "cpan" || Type == "vsm" || Type == "cran")
-            {
-                return name;
-            }
+            
             return name.ToLower();
         }
 
